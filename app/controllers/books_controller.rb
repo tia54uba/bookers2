a0dfc3784f2ec260897a2f85dee287c18786aebd
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-18 12:25:32 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
-  def index
-  end
-
+    def index
+   @book = Book.new
+     @user = current_user
+     @books = Book.all
+    end
+    
+    def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+    end
+    
   def show
   end
 
   def edit
   end
+  
+    private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
