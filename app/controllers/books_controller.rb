ead8888ecc8b560df6182ed25fe9fb6edb83610a
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-19 12:03:41 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,12 +15,18 @@ class BooksController < ApplicationController
     def show
     @book = Book.find(params[:id])
     @user = @book.user
-    
+    @newbook = Book.new
 
     end
 
-  def edit
-  end
+    def edit
+    end
+    
+    def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+    end
 
     private
 
