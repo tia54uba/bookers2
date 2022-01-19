5b8fbb211251f350a153bb94c58b3374896823bb
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-19 08:09:20 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,20 +4,24 @@ class BooksController < ApplicationController
      @user = current_user
      @books = Book.all
     end
-    
+
     def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
     end
+
+    def show
+    @book = Book.find(params[:id])
+    @user = @book.user
     
-  def show
-  end
+
+    end
 
   def edit
   end
-  
+
     private
 
   def book_params
