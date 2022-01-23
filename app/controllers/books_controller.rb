8b82cd1c2f73ed5736b63da4681c7abea204be14
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-23 06:57:44 +0000

    [Finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
    before_action :authenticate_user!
 
+   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
     def index
     @book = Book.new
      @user = current_user
@@ -51,6 +53,13 @@ class BooksController < ApplicationController
 
     def book_params
     params.require(:book).permit(:title, :body)
+    end
+
+    def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
     end
 
 end
