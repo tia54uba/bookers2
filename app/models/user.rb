599086d6d4c784a70fcdf6d0403a5128ce1d98ee
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-04-20 14:03:43 +0000

    update log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,14 @@ class User < ApplicationRecord
   end
   profile_image.variant(resize: size).processed
   end
+  
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 
 
   
