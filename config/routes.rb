fc110e99b553317278a261c44d67fe309f25e654
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-04-20 14:00:33 +0000

    add guest log in

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
   #get 'users/show'
   #get 'users/edit'
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
