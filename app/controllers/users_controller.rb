422edab98fab4a66ec49d8bd6178f0e3d287efb2
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-04-22 13:32:26 +0000

    update guest log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
    before_action :authenticate_user!
+   before_action :ensure_guest_user, only: [:edit]
 
   def index
     @user = current_user
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user == current_user
       render:edit
-    else 
+    else
       redirect_to user_path(current_user)
     end
   end
@@ -38,4 +39,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
+end
 end
